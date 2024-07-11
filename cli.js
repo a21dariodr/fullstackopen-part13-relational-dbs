466e42928313d23a7f68a6bd40e14dbf29cc2978
a21dariodr <a21dariodr@iessanclemente.net>
2024-07-11 22:31:04 +0200

    Finished exercise 13.4

--- cli.js
+++ cli.js
@@ -1,9 +1,7 @@
 require('dotenv').config()
 const { Sequelize, QueryTypes } = require('sequelize')
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    host: new URL(process.env.DATABASE_URL).hostname
-})
+const sequelize = new Sequelize(process.env.DATABASE_URL)
 
 const main = async () => {
     try {
