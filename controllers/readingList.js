2d5001acfb3bf8b75600409966c61d58803ff428
a21dariodr <a21dariodr@iessanclemente.net>
2024-07-17 18:36:34 +0200

    Finished exercise 13.22

--- controllers/readingList.js
+++ controllers/readingList.js
@@ -1,11 +1,46 @@
 const router = require('express').Router()
-const { ReadingList } = require('../models')
+const { ReadingList, User, Blog } = require('../models')
+const { tokenExtractor } = require('../util/middleware')
 
 router.post('/', async (req, res, next) => {
     try {
         const readingList = await ReadingList.create(req.body)
         console.log(readingList.toJSON())
         res.json(readingList)
+    } catch(error) {
+        next(error)
+    }
+    
+})
+
+router.put('/:id', tokenExtractor, async (req, res, next) => {
+    if (!req.decodedToken) res.status(404).end()
+    if (!('read' in req.body)) res.status(400).end()
+    
+    const user = await User.findByPk(req.decodedToken.id, {
+        attributes: ['id', 'name', 'username'],
+        include: {
+            model: Blog,
+            as: 'readings',
+            attributes: ['id', 'url', 'title', 'author', 'likes', 'year'],
+            through: {
+                attributes: ['read', 'id']
+            }
+        }
+    })
+    if (!user) res.status(404).end()
+
+    if (!user.readings.map(r => r.id).includes(Number(req.params.id))) {
+        const error = new Error('The blog is not in the user\'s reading list')
+        error.type = 'ReadStatusUpdateError'
+        next(error)
+    }
+
+    try {
+        const reading = user.readings.find(r => r.id === Number(req.params.id))
+        reading.readingList.read = req.body.read
+        await user.save()
+        res.json(user)
     } catch(error) {
         next(error)
     }
--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const errorHandler = (error, _request, response, next) => {
     'UserCreationError',
     'UserUpdateError',
     'TokenMissing',
-    'WrongUser'
+    'WrongUser',
+    'ReadStatusUpdateError'
   ]
 
   if (badRequestErrorTypes.includes(error.type)) {
